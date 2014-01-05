f583c2ec10eb67d9f2ba9d5d9a0bbda53cf6c627
Gregory Jefferis <jefferis@gmail.com>
2014-01-05 19:07:00 +0000

    add nat-package.R for docs

---
+++ R/nat-package.R
@@ -0,0 +1,18 @@
+#' Analyse 3D biological image data especially neurons
+#' 
+#' Key functions that you will want to look at include
+#' read.neuron
+#' write.neuron
+#' 
+#' Objects of class
+#' neuron
+#' neuronlist
+#' 
+#' plot.neuron
+#' plot3d.neuron
+#' @name nat-package
+#' @aliases nat
+#' @seealso \code{\link{rgl}} which is used for visualisation.
+#' @docType package
+#' @keywords package
+NULL
