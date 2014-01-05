37c38b08fb242223987a5a61816baea3b2dbbac9
Gregory Jefferis <jefferis@gmail.com>
2014-01-05 19:10:49 +0000

    add cmtkreg functions (handling CMTK registrations) + tests

---
+++ R/cmtkreg.R
@@ -0,0 +1,73 @@
+#' Create and test cmtkreg objects that specify path to a CMTK registration
+#' 
+#' @description \code{cmtkreg} creates an object of class \code{cmtkreg} that
+#'   describes one (or more) \href{www.nitrc.org/projects/cmtk/}{CMTK}
+#'   registrations. This is simply a character vector that also has class
+#'   cmtkreg.
+#' @param x Path to a cmtk registration (either plain character vector or 
+#'   cmtkreg object)
+#' @param returnDir Whether to return the registration directory (default) or 
+#'   the actual file containing the registration
+#' @export
+cmtkreg<-function(x, returnDir=TRUE){
+  if(length(x)>1) return(sapply(x,cmtkreg,returnDir=returnDir))
+  
+  if(!file.exists(x)) {
+    return(NA_character_)
+  }
+  
+  if(file_test("-d", x)){
+    regdir=x
+    # this is a directory, so see if we can find the registration
+    reg=dir(x,pattern="^registration(\\.gz){0,1}",full.names=T)[1]
+    if(is.na(reg)) 
+      stop(paste("Unable to find registration file in",regdir))
+  } else {
+    reg=x
+    regdir=dirname(x)
+  }
+  
+  as.cmtkreg(ifelse(returnDir,regdir,reg))
+}
+
+#' @description \code{as.cmtkreg} adds class \code{cmtkreg} to objects that do not
+#'   already inherit from it.
+#' @rdname cmtkreg
+#' @export
+as.cmtkreg<-function(x){
+  if(!inherits(x,'cmtkreg'))
+    class(x)=c("cmtkreg",x)
+  x
+}
+
+#' @description \code{is.cmtkreg} checks if an object is a cmtk registration either by checking 
+#'   class (default), or inspecting file.
+#' @param filecheck Whether to check object class only (default: 'none') or find
+#'   amd check if registration file \strong{exists} or check \strong{magic}
+#'   value in first line of file.
+#' @rdname cmtkreg
+#' @export
+is.cmtkreg<-function(x, filecheck=c('none','exists','magic')) {
+  filecheck=match.arg(filecheck, choices = c('none','exists','magic'))
+  if(filecheck=='none') return(inherits(x,'cmtkreg'))
+  if(!is.character(x)) return (FALSE)
+  
+  if(length(x)>1) return(sapply(x,is.cmtkreg,filecheck=filecheck))
+  
+  reg=cmtkreg(x, returnDir=FALSE)
+  if(filecheck=='exists') return(!is.na(reg))
+  # charToRaw('! TYPEDSTREAM')
+  cmtk.magic=as.raw(c(0x21, 0x20, 0x54, 0x59, 0x50, 0x45, 0x44, 0x53, 0x54, 
+                      0x52, 0x45, 0x41, 0x4d))
+  
+  magic=try({
+    gzf<-gzfile(reg,'rb')
+    magic<-readBin(gzf,what=raw(),n=length(cmtk.magic))
+    close(gzf)
+    magic},
+            silent = FALSE)
+
+  return(!inherits(magic,'try-error') && 
+           length(magic)==length(cmtk.magic) 
+         && all(magic==cmtk.magic))
+}
---
+++ inst/tests/test-cmtkreg.R
@@ -0,0 +1,42 @@
+context("Verify cmtkreg functions")
+require(testthat)
+require(nat.utils)
+
+test_that("is.cmtkreg works ok for objects", {
+  reg='missingregistration.list'
+  creg=as.cmtkreg('missingregistration.list')
+  expect_false(is.cmtkreg(reg))
+  expect_true(is.cmtkreg(creg))
+})
+
+test_that("(is.)cmtkreg works ok for files", {
+  reg2="../testdata/cmtk/dofv1.1wshears.list"
+  creg2=cmtkreg(reg2)
+  expect_true(is.cmtkreg(creg2))
+  expect_true(is.cmtkreg(creg2,filecheck='exists'))
+  expect_true(is.cmtkreg(creg2,filecheck='magic'))
+  
+  reg3=tempfile(fileext='.list')
+  on.exit(unlink(reg3,recursive=TRUE),add=TRUE)
+  dir.create(reg3)
+  expect_error(cmtkreg(reg3))
+  
+  # write registration file with good name and bad magic
+  reg3r=file.path(reg3,'registration')
+  writeLines(text='!TYPEDSTREAM\n',con=reg3r)
+  # this should work
+  creg3<-cmtkreg(reg3)
+  expect_true(is.cmtkreg(creg3))
+  expect_true(is.cmtkreg(reg3,filecheck='exists'))
+  # but magic is bad
+  expect_false(is.cmtkreg(reg3,filecheck='magic'))
+  # check handling of gzip file
+  unlink(reg3r)
+  reg3rgz=file.path(reg3,'registration.gz')
+  gzf=gzfile(reg3rgz,'w')
+  #on.exit(close(gzf),add=TRUE)
+  cat('! TYPEDSTREAM\n',file=gzf,useBytes=TRUE)
+  close(gzf)
+  expect_true(is.cmtkreg(reg3,filecheck='exists'))
+  expect_true(is.cmtkreg(reg3,filecheck='magic'))
+})
