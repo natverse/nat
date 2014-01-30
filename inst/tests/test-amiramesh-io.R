85d5fe680be558e36ceb91ae9574d04a4be30683
Gregory Jefferis <jefferis@gmail.com>
2014-01-30 20:09:18 +0000

    tests for new read.amiramesh hxzip/rle functionality

--- inst/tests/test-amiramesh-io.R
+++ inst/tests/test-amiramesh-io.R
@@ -9,3 +9,12 @@ test_that("check basic reading of amiramesh file (neuron)",{
   am='../testdata/neuron/EBT7R.am'
   expect_is(read.amiramesh(am),'list')
 })
+
+test_that("check reading amiramesh in zlib and rle formats",{
+  rleam='../testdata/amira/LHMask.Labels.rle.am'
+  zipam='../testdata/amira/LHMask.zip.am'
+  expect_is(rle<-read.amiramesh(rleam), 'array')
+  expect_that(dim(rle), is_equivalent_to(c(50, 50, 50)))
+  expect_is(zip<-read.amiramesh(zipam), 'array')
+  expect_that(dim(zip), is_equivalent_to(c(50, 50, 50)))
+})
