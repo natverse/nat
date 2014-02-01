1ffd07fb0ec9aed5f0bf36a39b812c087dbf1797
Gregory Jefferis <jefferis@gmail.com>
2014-02-01 22:07:34 +0000

    basic implementation of new read.neuron.hxskel function

--- R/neuron-io.R
+++ R/neuron-io.R
@@ -223,3 +223,50 @@ read.neuron.swc<-function(f, ...){
   d$W=d$W*2
   as.neuron(d, InputFileName=f, ...)
 }
+
+# read neurons in the format produced by Amira skeletonize plugin
+read.neuron.hxskel<-function(file, ...){
+  ndata=read.amiramesh(file)
+  required_fields=c("Coordinates", "NeighbourCount", "Radii", "NeighbourList")
+  missing_fields=setdiff(required_fields,names(ndata))
+  if(length(missing_fields))
+    stop("Neuron: ",file," is missing fields: ",paste(missing_fields,collapse=" "))
+  
+  d=data.frame(ndata$Coordinates)
+  colnames(d)=c("X","Y","Z")
+  d$W=ndata$Radii*2
+  d$NeighbourCount=ndata$NeighbourCount
+  nVertices=nrow(d)
+  d$PointNo=seq(nVertices)
+  d[,1:4]=round(d[,1:4],digits=3)
+  # Note these numbers come in zero indexed, but I will want them 1-indexed
+  Neighbours=data.frame(Neighbour=ndata$NeighbourList+1,CurPoint=rep(seq(nVertices),d$NeighbourCount))
+  Origin=ndata$Origins
+  if(!is.null(Origin)) Origin=Origin+1
+  
+  # Handle materials
+  if(length(ndata$vertexTypeList)){
+    SegmentProps=data.frame(
+      PointNo=rep(seq(nVertices),ndata$vertexTypeCounter),
+      Id=ndata$vertexTypeList)
+    if(length(attr(ndata,'Materials'))){
+      # we have named materials, so let's add them as an attribute
+      attr(SegmentProps,'Materials')=attr(ndata,'Materials')
+    }
+    # we can only add one numeric label to the SWC format version of the neuron
+    FirstSegmentProps=subset(SegmentProps,!duplicated(PointNo))
+    # 0 = undefined
+    d$Label=0L
+    d[FirstSegmentProps$PointNo,"Label"]=FirstSegmentProps$Id
+  }
+
+  el=data.matrix(Neighbours)
+  # make a doubly linked graph from this double edge list
+  doubleg=ngraph(el, d$PointNo, directed=TRUE)
+  # TODO see if we can make appropriate directed graph rather than converting
+  # to undirected.
+  ug=as.undirected(doubleg, mode='collapse')
+  if(!inherits(ug,'ngraph')) class(ug)=c("ngraph",class(ug))
+  n=as.neuron(ug, vertexData=d, origin=Origin, ... )
+  n
+}
