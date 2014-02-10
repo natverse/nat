37afc591540d5f130e1f0051e8821d571c2761a7
Gregory Jefferis <jefferis@gmail.com>
2014-02-10 16:08:08 +0000

    Direct assignment when registering read/write functions
    * quoting the functions can help debugging
    * but may cause visibility problems since they are not currently
      exported from nat.
    * in general functions must be one of
      - directly assigned
      - named and exported i.e. visible on R's search path

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,10 @@
   registerformat('swc', read=read.neuron.swc, write=write.neuron.swc, 
                  class='neuron')
   registerformat('rds', read=readRDS, write=saveRDS, class='neuron')
-  registerformat('hxskel', ext='.am', read="read.neuron.hxskel", 
-                 write='write.neuron.hxskel', magic=is.hxskel,
+  registerformat('hxskel', ext='.am', read=read.neuron.hxskel, 
+                 write=write.neuron.hxskel, magic=is.hxskel,
                  class='neuron', magiclen=11)
-  registerformat('hxlineset', ext='.am', read="read.neuron.hxlineset", 
+  registerformat('hxlineset', ext='.am', read=read.neuron.hxlineset, 
                  write=write.neuron.hxlineset, magic=is.hxlineset,
                  class='neuron', magiclen=11)
   invisible()
