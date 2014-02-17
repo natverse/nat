778fd818e6af980a01b5619a1073030bd88450cf
Gregory Jefferis <jefferis@gmail.com>
2014-02-17 13:15:32 +0000

    inactivate remote neuronlistfh test
    * we shouldn't really be doing this as a default package test

--- inst/tests/test-neuronlistfh.R
+++ inst/tests/test-neuronlistfh.R
@@ -57,6 +57,6 @@ test_that("Can download a neuronlistfh object with MD5'd objects", {
   localdir <- tempfile()
   dir.create(localdir)
   on.exit(unlink(localdir, recursive=TRUE))
-  kcs20md5 <- read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/kcs20.rds", localdir=localdir)
-  expect_equal(dim(kcs20md5[[1]]$points), c(284, 3))
+#   kcs20md5 <- read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/kcs20.rds", localdir=localdir)
+#   expect_equal(dim(kcs20md5[[1]]$points), c(284, 3))
 })
