d2f68f7d8d0ffe17b74fcb096c000d54628ccb67
Ashley Manton <ajd.manton@googlemail.com>
2014-02-18 15:53:36 +0000

    Change test setup so that tests involving remote download do not run on CRAN

--- inst/tests/test-neuronlistfh.R
+++ inst/tests/test-neuronlistfh.R
@@ -52,11 +52,3 @@ test_that("Can create a neuronlistfh with a hashmap",{
   expect_equal(lapply(kcs20fh,length),lapply(kcs20,length))
   expect_equal(sapply(kcs20fh,length),sapply(kcs20,length))
 })
-
-test_that("Can download a neuronlistfh object with MD5'd objects", {
-  localdir <- tempfile()
-  dir.create(localdir)
-  on.exit(unlink(localdir, recursive=TRUE))
-#   kcs20md5 <- read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/kcs20.rds", localdir=localdir)
-#   expect_equal(dim(kcs20md5[[1]]$points), c(284, 3))
-})
