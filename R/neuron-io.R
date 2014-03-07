953c2f789bdc8498ac1f0029a7932ae2152855ec
Gregory Jefferis <jefferis@gmail.com>
2014-03-07 22:21:00 +0000

    document ability of read.neurons to use neuronlistfh object as input

--- R/neuron-io.R
+++ R/neuron-io.R
@@ -42,32 +42,34 @@ read.neuron<-function(f, format=NULL, ...){
 #' Read one or more neurons from file to a neuronlist in memory
 #' 
 #' @details This function will cope with the same set of file formats offered by
-#'   \code{read.neuron}. If the \code{paths} argument specifies a (single)
-#'   directory then all files in that directory will be read unless an optional
-#'   regex pattern is also specified. \code{neuronnames} must specify a unique
+#'   \code{read.neuron}. If the \code{paths} argument specifies a (single) 
+#'   directory then all files in that directory will be read unless an optional 
+#'   regex pattern is also specified. \code{neuronnames} must specify a unique 
 #'   set of names that will be used as the names of the neurons in the resultant
 #'   neuronlist. If \code{neuronnames} is a a function then this will be applied
-#'   to the path to each neuron. The default value is the function
+#'   to the path to each neuron. The default value is the function 
 #'   \code{basename} which results in each neuron being named for the input file
-#'   from which it was read. The optional dataframe (\code{df}) detailing each
-#'   neuron should have \code{rownames} that match the names of each neuron. It
-#'   would also make sense if the same key was present in a column of the data
-#'   frame. If the dataframe contains more rows than neurons, the superfluous
-#'   rows are dropped with a warning. If the dataframe is missing rows for some
-#'   neurons an error is generated. If SortOnUpdate is TRUE then updating an
-#'   existing neuronlist should result in a new neuronlist with ordering
+#'   from which it was read. The optional dataframe (\code{df}) detailing each 
+#'   neuron should have \code{rownames} that match the names of each neuron. It 
+#'   would also make sense if the same key was present in a column of the data 
+#'   frame. If the dataframe contains more rows than neurons, the superfluous 
+#'   rows are dropped with a warning. If the dataframe is missing rows for some 
+#'   neurons an error is generated. If SortOnUpdate is TRUE then updating an 
+#'   existing neuronlist should result in a new neuronlist with ordering 
 #'   identical to reading all neurons from scratch.
-#' @param paths Paths to neuron input files (or directory containing neurons)
-#' @param pattern If paths is a directory, regex that file names must match.
+#' @param paths Paths to neuron input files \emph{or} a directory containing 
+#'   neurons \emph{or} a \code{\link{neuronlistfh}} object.
+#' @param pattern If paths is a directory, \link{regex} that file names must 
+#'   match.
 #' @param neuronnames Character vector or function that specifies neuron names
-#' @param format File format for neuron (see \code{read.neuron})
+#' @param format File format for neuron (see \code{\link{read.neuron}})
 #' @param df Optional data frame containing information about each neuron
 #' @param OmitFailures Omit failures (when TRUE) or leave an NA value in the 
 #'   list
 #' @param SortOnUpdate Sort the neuronlist when update adds new neurons
 #' @param nl An existing neuronlist to be updated (see details)
 #' @param ... Additional arguements to passed to read.neuron methods
-#' @return \code{neuronlist} object containing the neurons
+#' @return \code{\link{neuronlist}} object containing the neurons
 #' @export
 #' @seealso \code{\link{read.neuron}}
 #' @family neuronlist
