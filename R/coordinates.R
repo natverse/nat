a45a9b828a15dd3a252c5caf6a5b83978e22fcb5
James Manton <ajd.manton@googlemail.com>
2014-04-04 17:18:14 +0100

    Add coord2ind() + test

--- R/coordinates.R
+++ R/coordinates.R
@@ -1,3 +1,60 @@
+#' Find 1D indices into a 3D image given spatial coordinates
+#' 
+#' @param coords spatial coordinates of image voxels.
+#' @export
+coord2ind <- function(coords, ...) UseMethod("coord2ind")
+
+
+#' @param imdims array dimensions of 3D image.
+#' @param voxdims vector of 3 voxels dimensions (width, height, depth).
+#' @param aperm permutation order for axes.
+#' @param Clamp ???
+#' @param CheckRanges whether to check if coordinates are out of range.
+#' @seealso \code{\link{ind2coord}}, \code{\link{ind2sub}}
+#' @S3method coord2ind default
+#' @rdname coord2ind
+coord2ind.default<-function(coords,imdims,voxdims=NULL,origin=NULL,aperm,Clamp=FALSE,CheckRanges=!Clamp){
+  if(inherits(imdims, 'im3d')) {
+    voxdims <- voxdims(imdims)
+    origin <- origin(imdims)
+    imdims <- dim(imdims)
+  }
+  else if(is.array(imdims)){
+    if(missing(voxdims))
+      voxdims=as.numeric(voxdim(imdims))
+    if(missing(origin))
+      origin=getBoundingBox(imdims)[c(1,3,5)]
+    imdims=dim(imdims)
+  }
+  
+  if(length(imdims) != 3)
+    stop('coord2ind only handles 3d data')
+  
+  if(!is.matrix(coords))
+    coords=matrix(coords,byrow=TRUE,ncol=length(coords))
+  if(!missing(origin))
+    coords=t(t(coords)-origin)
+  
+  pixcoords=t(round(t(coords)/voxdims))+1
+  
+  # make sure no points are out of range
+  if(Clamp){
+    pixcoords[,1]=pmin(imdims[1],pmax(1,pixcoords[,1]))
+    pixcoords[,2]=pmin(imdims[2],pmax(1,pixcoords[,2]))
+    pixcoords[,3]=pmin(imdims[3],pmax(1,pixcoords[,3]))
+  } else if(CheckRanges){
+    ranges=apply(pixcoords,2,range)
+    if(any(ranges[2,]>imdims) || any(ranges[1,]<1))
+      stop("pixcoords out of range")
+  }
+  
+  # convert to 1d indices
+  if (!missing(aperm))
+    imdims=imdims[aperm]
+  sub2ind(imdims,pixcoords)
+}
+
+
 #' Find 1D index given n-dimensional indices
 #' 
 #' Emulates the MATLAB function \code{sub2ind}.
--- tests/testthat/test-coordinates.R
+++ tests/testthat/test-coordinates.R
@@ -7,3 +7,10 @@ test_that("sub2ind returns correct indices", {
   inds <- apply(subs, 2, function(x) sub2ind(indices=x, dims=dim(data.array)))
   expect_equal(data[20:40], data[inds[20:40]])
 })
+
+test_that("coord2ind returns correct coordinates", {
+  testImage <- read.im3d("testdata/nrrd/LHMask.nrrd")
+  ind <- coord2ind(matrix(c(10, 20, 30, 11, 20, 30), nrow=2, byrow=TRUE), testImage)
+  ind.expected <- c(53208, 53209)
+  expect_equal(ind, ind.expected)
+})
