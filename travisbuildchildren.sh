5dafb82da5ee0219a9e0569db5296a9374df7dc1
James Manton <ajd.manton@googlemail.com>
2014-05-02 16:48:40 +0100

    Build nat.nblast on Travis after successful nat build

--- travisbuildchildren.sh
+++ travisbuildchildren.sh
@@ -9,3 +9,8 @@ curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Autho
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.flybrains/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 # Restart last nat.flybrains build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+
+# Get last nat.nblast build number
+BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.nblast/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+# Restart last nat.nblast build
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
