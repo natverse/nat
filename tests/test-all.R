ce038e0729969bd2e14364fa6add7f89d691b0ed
Gregory Jefferis <jefferis@gmail.com>
2014-06-01 11:28:47 -0400

    suppress remote tests if flybrain can’t be reached
    * nb introduces httr in suggests

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,11 +1,14 @@
 library(testthat)
 library(nat)
+library(httr)
 
-if(Sys.getenv('NOT_CRAN') == "true") {
+if(Sys.getenv('NOT_CRAN') == "true" && 
+     isTRUE(try(url_ok('http://flybrain.mrc-lmb.cam.ac.uk/')))) {
   # Run all tests
   test_check("nat")
 } else {
-  # We're on CRAN, so don't run anything involving remote files
+  # We're on CRAN or flybrain is inacessible, so don't run anything involving 
+  # remote files
   regex <- "^[^.]+"
   test_check("nat", filter=regex)
 }
