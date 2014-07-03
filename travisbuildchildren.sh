4eae4dd6e72087d565f1ef1b7910d05d7d43575a
James Manton <ajd.manton@googlemail.com>
2014-07-03 14:14:38 +0100

    Trigger rebuild of nat.templatebrains on successful Travis build

--- travisbuildchildren.sh
+++ travisbuildchildren.sh
@@ -5,6 +5,11 @@ BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferis/flycircuit/builds'
 # Restart last flycircuit build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
 
+# Get last nat.templatebrains build number
+BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.templatebrains/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+# Restart last nat.templatebrains build
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+
 # Get last nat.flybrains build number
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.flybrains/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 # Restart last nat.flybrains build
