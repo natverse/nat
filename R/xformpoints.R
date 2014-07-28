e6d5f9d4b5757c64114f4c33ed3fd3060dfd7092
Gregory Jefferis <jefferis@gmail.com>
2014-07-28 17:01:25 +0100

    CHECK: partial arg match fix

--- R/xformpoints.R
+++ R/xformpoints.R
@@ -61,7 +61,7 @@ xformpoints.cmtkreg<-function(reg, points, transformtype=c('warp','affine'),
   transformtype=match.arg(transformtype)
   direction=match.arg(direction,c("inverse",'forward'),several.ok=TRUE)
   
-  if(length(reg)>1 && !cmtk.version(min='3.2.2')){
+  if(length(reg)>1 && !cmtk.version(minimum = '3.2.2')){
     # there is a bug in applying compound registrations in CMTK<=3.2.1
     # see https://github.com/jefferis/cmtk/commit/209168d892d8980e47
     return(mapply(xformpoints.cmtkreg, reg=reg, direction=direction, 
