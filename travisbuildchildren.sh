5ac1020a9695addb2db9a726c40f63af62b947c5
James Manton <ajd.manton@googlemail.com>
2014-08-04 11:29:40 +0100

    Remove nat.flybrains from travisbuildchildren.sh script
    nat.templatebrains now triggers a rebuild of nat.flybrains

--- travisbuildchildren.sh
+++ travisbuildchildren.sh
@@ -10,11 +10,6 @@ BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.templatebra
 # Restart last nat.templatebrains build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
 
-# Get last nat.flybrains build number
-BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.flybrains/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-# Restart last nat.flybrains build
-curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
-
 # Get last nat.nblast build number
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/jefferislab/nat.nblast/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 # Restart last nat.nblast build
