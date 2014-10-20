f6058f1d3c284fe1de916860887bd83a75361381
Gregory Jefferis <jefferis@gmail.com>
2014-10-20 18:13:40 +0100

    add process_neuroml_cell
    * to process the XML tree for a single cell

--- R/neuron-io-neuroml.R
+++ R/neuron-io-neuroml.R
@@ -13,3 +13,74 @@ read.neuroml<-function(f, ...){
   names(cells)<-cell_names
   invisible(cells)
 }
+
+# process the xml tree for a (morphml) format cell
+process_neuroml_cell<-function(cell, ...) {
+  if(!all(c("segments",'cables') %in% names(cell))){
+    stop("Each cell must contain segments and cables elements")
+  }
+  
+  ## process morphml segments
+  # first segment attributes
+  segs=XML::xmlChildren(cell[['segments']])
+  # only keep children called "segment"
+  segs=segs[names(segs)=='segment']
+  seginfo=t(sapply(segs, function(x) {
+      atts=XML::xmlAttrs(x)
+      if(length(atts)==3) atts['parent']=-1
+      atts[c('id','cable','parent','name')]
+    } ))
+  rownames(seginfo)=as.vector(seginfo[,'id'])
+  int_cols=c("id","cable","parent")
+  seginfo_int=seginfo[,int_cols]
+  mode(seginfo_int)='integer'
+  
+  segdf=as.data.frame(seginfo_int)
+  other_cols=setdiff(colnames(seginfo), int_cols)
+  segdf[,other_cols]=seginfo[,other_cols]
+  
+  # extract proximal and distal info for each segment
+  # and convert to data.frame
+  proxinfo=t(sapply(segs, function(x) {
+    proxi=XML::xmlChildren(x)[['proximal']]
+    if(is.null(proxi)) rep(NA_real_, 4)
+    else as.numeric(XML::xmlAttrs(proxi))}))
+  colnames(proxinfo)=paste0(c("x", "y", "z", "diameter"),".p")
+  
+  distinfo=t(sapply(segs, function(x) {
+    distal=XML::xmlChildren(x)[['distal']]
+    if(is.null(distal)) rep(NA_real_, 4)
+    else as.numeric(XML::xmlAttrs(distal))}))
+  colnames(distinfo)=paste0(c("x", "y", "z", "diameter"),".d")
+  
+  segdf[,colnames(proxinfo)]=proxinfo
+  segdf[,colnames(distinfo)]=distinfo
+
+  ## extract cable info
+  cables=XML::xmlChildren(cell[['cables']])
+  # only keep children called "cable"
+  cables=cables[names(cables)=='cable']
+  
+  cableinfo=t(sapply(cables, function(x) {
+    atts=XML::xmlAttrs(x)
+    rval=c(id=NA_integer_, name=NA_character_, fract_along_parent=NA_real_)
+    rval['id']=atts['id']
+    rval['name']=atts['name']
+    if('fract_along_parent'%in%atts['fract_along_parent']){
+      rval['fract_along_parent']=as.numeric(atts['fract_along_parent'])
+    }
+    rval
+  }))
+  rownames(cableinfo)=as.vector(cableinfo[,'id'])
+  special_cols=c("id","fract_along_parent")
+  
+  cabledf=data.frame(id=as.integer(cableinfo[,'id']), 
+                     fract_along_parent=as.numeric(cableinfo[,'fract_along_parent']))
+  cabledf=cbind(cabledf,cableinfo[,!colnames(cableinfo)%in%special_cols])
+  # cable type
+  cabledf$type=sapply(cables, function(x) XML::xmlValue(x)[1])
+  cabledf
+  
+  # return a list of class nml_cell with segment/cable information
+  structure(list(segments=segdf, cables=cabledf), class='nml_cell')
+}
