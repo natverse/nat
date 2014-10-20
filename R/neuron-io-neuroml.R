7f050b45b724f6918a374ede6716abacdea76aaa
Gregory Jefferis <jefferis@gmail.com>
2014-10-20 18:11:16 +0100

    rename to read.neuroml since this is all it does so far
    * fix typo
    * also ensure all xml functions are prefixed with XML::

--- R/neuron-io-neuroml.R
+++ R/neuron-io-neuroml.R
@@ -1,14 +1,14 @@
-read.neuron.neuroml<-function(f, ...){
+read.neuroml<-function(f, ...){
   # basic parsing of xml doc (using libxml)
   doc=try(XML::xmlParse(f))
   if(inherits(doc, 'try-error')) stop("Unable to parse file as neuroml")
-  ns=xmlNamespaceDefinitions(doc)
+  ns=XML::xmlNamespaceDefinitions(doc)
   defaultns=try(ns[[1]]$uri)
-  if(inherits(if(inherits(defaultns, 'try-error'))
+  if(inherits(defaultns, 'try-error'))
     stop("Unable to identify default neuroml namespace")
   
   # fetch cells 
-  cells=getNodeSet(doc,'//*/nml:cell', c(nml=defaultns)
+  cells=XML::getNodeSet(doc,'//*/nml:cell', c(nml=defaultns))
   cell_names=XML::xmlSApply(cells, function(x) XML::xmlAttrs(x)['name'])
   names(cells)<-cell_names
   invisible(cells)
