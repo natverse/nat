29cdd7317a2025a1b442e300e1cee0e2b362fe61
Gregory Jefferis <jefferis@gmail.com>
2014-10-20 12:32:35 +0100

    ensure we identify default namespace

--- R/neuron-io-neuroml.R
+++ R/neuron-io-neuroml.R
@@ -1,7 +1,14 @@
 read.neuron.neuroml<-function(f, ...){
+  # basic parsing of xml doc (using libxml)
   doc=try(XML::xmlParse(f))
   if(inherits(doc, 'try-error')) stop("Unable to parse file as neuroml")
-  cells=getNodeSet(doc,'//*/nml:cell', c(nml="http://morphml.org/neuroml/schema"))
+  ns=xmlNamespaceDefinitions(doc)
+  defaultns=try(ns[[1]]$uri)
+  if(inherits(if(inherits(defaultns, 'try-error'))
+    stop("Unable to identify default neuroml namespace")
+  
+  # fetch cells 
+  cells=getNodeSet(doc,'//*/nml:cell', c(nml=defaultns)
   cell_names=XML::xmlSApply(cells, function(x) XML::xmlAttrs(x)['name'])
   as.vector(cell_names)
 }
