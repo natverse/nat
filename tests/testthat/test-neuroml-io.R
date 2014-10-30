424e54db887e722d6cac156a2fd0d4a9da7c0507
Gregory Jefferis <jefferis@gmail.com>
2014-10-30 19:07:07 +0000

    suppress warnings in neuroml tests
    * relating to handling of fract_along_parent

--- tests/testthat/test-neuroml-io.R
+++ tests/testthat/test-neuroml-io.R
@@ -3,7 +3,10 @@ context("NeuroML input output")
 test_that("read neuroml files", {
   nml_level1_files=dir("testdata/neuroml/level1", pattern = '[xn]ml$', full.names = T)
   for (f in nml_level1_files) {
-    expect_is(read.neuron.neuroml(f), 'neuron', info = paste("file:",basename(f)))
+    # suppress warnings re cable segments
+    suppressWarnings(
+      expect_is(read.neuron.neuroml(f), 'neuron', info = paste("file:",basename(f)))
+    )
   }
 })
 
@@ -19,7 +22,10 @@ test_that("parse neuroml files", {
   
   for (swc in swcs) {
     nml=paste0(tools::file_path_sans_ext(swc),".xml")
-    expect_equal(read.neuron.neuroml(nml), read.neuron(swc), info = basename(nml))
+    # suppress warnings re cable segments
+    suppressWarnings(
+      expect_equal(read.neuron.neuroml(nml), read.neuron(swc), info = basename(nml))
+    )
   }
 })
 
