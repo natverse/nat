1b07f22ef76c0c2c91adb20e99b998fc4bb98bbc
Gregory Jefferis <jefferis@gmail.com>
2014-11-11 19:29:59 +0000

    teach seglist2swc to accept matrices on input
    * i.e. not just data.frames

--- R/seglist.R
+++ R/seglist.R
@@ -183,7 +183,7 @@ seglist2swc<-function(x, d, RecalculateParents=TRUE, ...){
     if(!is.null(sl[[1]][[1]]))
       sl=unlist(sl, recursive=FALSE)
   }
-  
+  if(!is.data.frame(d)) d=as.data.frame(d)
   if(is.null(d$PointNo) && nrow(d)==0)
     stop("Must supply either supply some coords or PointNos")
   d=normalise_swc(d, ...)
