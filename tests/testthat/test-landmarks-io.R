2c0c80436efc245166f3f73a381755fcdddb169b
Gregory Jefferis <jefferis@gmail.com>
2014-11-23 22:18:34 +0000

    test reading fiji landmarks

--- tests/testthat/test-landmarks-io.R
+++ tests/testthat/test-landmarks-io.R
@@ -20,3 +20,15 @@ test_that("we can read/write paired Amira Landmarks",{
   names(testData.new)<-NULL
   expect_equal(testData,testData.new,tol=1e-6)	
 })
+
+test_that("we can read Fiji landmarks", {
+  baseline=structure(c(256.922344, 200.312336, 441.060392,
+                       11.819672, 92.691112, 127.52804, 
+                       66.563416, 84.603968, 46.034512),
+                     .Dim = c(3L, 3L), .Dimnames = list(
+                         c("right_alpha_tip", "right_tract_cross_vLH", 
+                           "left_vlpr_tract_crossing"),
+                         c("X", "Y", "Z")))
+  
+  expect_equal(read.landmarks.fiji("testdata/landmarks/JFRC2.points"), baseline)
+})
