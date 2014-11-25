5a75595dd78f07a6ff4d3d0dab2800ac0e375a1d
Gregory Jefferis <jefferis@gmail.com>
2014-11-25 22:04:24 +0000

    additional test for xformimage

--- tests/testthat/test-xformimage.R
+++ tests/testthat/test-xformimage.R
@@ -1,16 +1,18 @@
 context("xformimage")
 
-
 if(!is.null(cmtk.bindir())){
   test_that("xform(image) can carry out a simple reformat operation", {
     td<-tempfile(pattern = 'xformimage_test')
     dir.create(td)
     on.exit(unlink(td))
     xformt=expression(xform("testdata/nrrd/JFRC2-444_mask.nrrd", 
-                                     reg = 'testdata/cmtk/FCWB_JFRC2_01_warp_level-01.list/', 
-                                     target='testdata/nrrd/FCWB_2um_mask.nrrd',
-                                     out=td, Verbose=FALSE))
+                            reg = 'testdata/cmtk/FCWB_JFRC2_01_warp_level-01.list/', 
+                            target='testdata/nrrd/FCWB_2um_mask.nrrd',
+                            out=td, Verbose=FALSE))
     expect_equal(basename(out<-eval(xformt)), "FCWB_2um_mask-JFRC2-444_mask.nrrd")
     
+    # verify bounding box of output
+    bb_out=structure(c(0, 561.9999, 0, 326.0003, 0, 106), .Dim = 2:3, class = "boundingbox")
+    expect_equal(boundingbox(read.im3d(out, ReadData = F)), bb_out)
   })
 }
