bb791a22333a3bc3560acbbc87d65e9bfbc29a67
Gregory Jefferis <jefferis@gmail.com>
2015-02-11 18:26:46 -0800

    additional tests for all.equal.neuron (err)
    * catches logic bug

--- tests/testthat/test-neuron.R
+++ tests/testthat/test-neuron.R
@@ -101,6 +101,16 @@ test_that("we can set NeuronName when there is no input file",{
   expect_equal(n$NeuronName, "MySpecialNeuron")
 })
 
+context("all.equal.neuron")
+test_that("all.equal.neuron behaves", {
+  n=Cell07PNs[[1]]
+  expect_equal(n, Cell07PNs[[1]])
+  n2=n
+  n2$NeuronName=NULL
+  expect_false(all.equal(n, n2, fieldsToCheck=NA))
+  expect_equal(n, n2, CheckSharedFieldsOnly=TRUE)
+})
+
 context("neuron arithmetic")
 
 test_that("operator equivalence for neuron arithmetic", {
