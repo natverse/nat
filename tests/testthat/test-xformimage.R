4bb1f7a68d7fff2a18788b10c3cab707c11bd9ac
Gregory Jefferis <jefferis@gmail.com>
2015-02-12 21:26:54 -0800

    complete coverage for xformimage
    * and reveal a bug

--- tests/testthat/test-xformimage.R
+++ tests/testthat/test-xformimage.R
@@ -14,7 +14,7 @@ if(!is.null(cmtk.bindir())){
     # check that we reformatted as expected
     imout=read.im3d(out)
     baseline=structure(c(1904662L, 592730L), .Dim = 2L, .Dimnames = structure(list(
-    imout = c("0", "255")), .Names = "imout"), class = "table")
+      imout = c("0", "255")), .Names = "imout"), class = "table")
     expect_equal(table(imout), baseline)
     # verify bounding box of output
     bb_out=structure(c(0, 561.9999, 0, 326.0003, 0, 106), .Dim = 2:3, class = "boundingbox")
@@ -39,5 +39,33 @@ if(!is.null(cmtk.bindir())){
     # verify bounding box of output
     bb_out=structure(c(0, 561.9999, 0, 326.0003, 0, 106), .Dim = 2:3, class = "boundingbox")
     expect_equal(boundingbox(imout), bb_out)
+    md5.1=tools::md5sum(file=out)
+    
+    ident.mat=cmtkparams2affmat()
+    xformt=expression(xform("testdata/nrrd/JFRC2-444_mask.nrrd", 
+                            reg = ident.mat,
+                            target='testdata/nrrd/FCWB_2um_mask.nrrd',
+                            interpolation="nn",
+                            out=td, Verbose=F))
+    expect_equal(basename(out<-eval(xformt)), "FCWB_2um_mask-JFRC2-444_mask.nrrd")
+    # check output files are identical
+    expect_equal(tools::md5sum(file=out), md5.1)
+    
+    # error tests
+    expect_error(xform("testdata/nrrd/JFRC2-444_mask.nrrd", 
+                       reg = list("bogusregistration"),
+                       target='testdata/nrrd/FCWB_2um_mask.nrrd',
+                       interpolation="nn",
+                       out=td, Verbose=F),
+                 "Unrecognised.* type")
+    
+    expect_error(xform("testdata/nrrd/JFRC2-444_mask.nrrd", 
+                       reg = "testdata/cmtk/FCWB_JFRC2_01_warp_level-01.list",
+                       transformtype = "affine",
+                       target='testdata/nrrd/FCWB_2um_mask.nrrd',
+                       interpolation="nn",
+                       out=td, Verbose=F),
+                 "Not yet implemented")
+    
   })
 }
