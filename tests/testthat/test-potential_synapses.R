a8b7e1b837e2d4b209c7ff0a9ce63bae8086a1af
Gregory Jefferis <jefferis@gmail.com>
2015-02-12 20:42:56 -0800

    complete coverage of dist3D_Segment_to_Segment
    * by testing neuron against itself i.e. with many
      parallel segments

--- tests/testthat/test-potential_synapses.R
+++ tests/testthat/test-potential_synapses.R
@@ -2,6 +2,7 @@ context("potential synapses")
 
 test_that("can call potential synapses on some neurons",{
   potential_synapses(Cell07PNs[[1]], Cell07PNs[[2]], s=2)
+  potential_synapses(Cell07PNs[[1]], Cell07PNs[[1]], s=2)
   potential_synapses(Cell07PNs[[1]], Cell07PNs[[2]], s=2, method='approx')
 })
 
