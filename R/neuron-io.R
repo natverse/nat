7e67e4234f6a41cd8d41c3777ef164fd7100f416
Gregory Jefferis <jefferis@gmail.com>
2015-04-18 10:29:11 +0100

    example of attaching a data.frame in read.neurons
    * closes #219

--- R/neuron-io.R
+++ R/neuron-io.R
@@ -131,11 +131,24 @@ read.neuron<-function(f, format=NULL, ...){
 #' @examples
 #' \dontrun{
 #' ## Read C. elegans neurons from OpenWorm github repository
-#' vds=paste0("https://raw.githubusercontent.com/openworm/CElegansNeuroML/",
-#'   "103d500e066125688aa7ac5eac7e9b2bb4490561/CElegans/generatedNeuroML/VD",
-#'   1:13,".morph.xml")
-#' vdnl=read.neurons(vds)
+#' vds=paste0("VD", 1:13)
+#' vdurls=paste0("https://raw.githubusercontent.com/openworm/CElegansNeuroML/",
+#'   "103d500e066125688aa7ac5eac7e9b2bb4490561/CElegans/generatedNeuroML/",vds,
+#'   ".morph.xml")
+#' vdnl=read.neurons(vdurls, neuronnames=vds)
 #' plot3d(vdnl)
+#' 
+#' ## The same, but this time add some metadata to neuronlist
+#' # fetch table of worm neurons from wormbase
+#' library(rvest)
+#' wormneurons = html_table(html("http://wormatlas.org/neurons/Individual%20Neurons/Neuronframeset.html"), fill=TRUE)[[4]]
+#' vddf=subset(wormneurons, Neuron%in%vds)
+#' rownames(vddf)=vddf$Neuron
+#' # attach metadata to neuronlist
+#' vdnl=read.neurons(vdurls, neuronnames=vds, df=vddf)
+#' # use metadata to plot a subset of neurons
+#' clear3d()
+#' plot3d(vdnl, grepl("P[1-6].app", Lineage))
 #' }
 read.neurons<-function(paths, pattern=NULL, neuronnames=basename, format=NULL,
                        nl=NULL, df=NULL, OmitFailures=TRUE, SortOnUpdate=FALSE,
