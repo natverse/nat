18100025b2211b0f3c9f44f8d03680ac64d424f6
Gregory Jefferis <jefferis@gmail.com>
2015-05-29 12:13:09 +0100

    remove useless endian from LHMask
    * harmless but was being removed by write.nrrd precisey because irrelevant
    * resulting in test failure
    * update test baseline for LHMask

--- tests/testthat/test-nrrd-io.R
+++ tests/testthat/test-nrrd-io.R
@@ -34,14 +34,12 @@ test_that("read.nrrd.header works",{
   origlhmaskfile="testdata/nrrd/LHMask.nrrd"
   expect_is(h<-read.nrrd.header(origlhmaskfile),'list')
   
-  baseh=structure(list(type = "uint8", encoding = "gzip", endian = "big", 
-      dimension = 3, sizes = c(50, 50, 50), `space dimension` = 3, 
-      `space directions` = structure(c(1.39999997615814, 0, 0, 
-      0, 1.39999997615814, 0, 0, 0, 1.39999997615814), .Dim = c(3L, 
-      3L)), `space origin` = c(0, 0, 0), `space units` = c("microns", 
-      "microns", "microns")), .Names = c("type", "encoding", "endian", 
-  "dimension", "sizes", "space dimension", "space directions", 
-  "space origin", "space units"))
+  baseh=list(type = "uint8", encoding = "gzip", dimension = 3, 
+             sizes = c(50, 50, 50), 
+             `space dimension` = 3, 
+             `space directions` = diag(1.39999997615814, 3),
+             `space origin` = c(0, 0, 0), 
+             `space units` = c("microns", "microns", "microns"))
   
   expect_equivalent(h, baseh)
 })
