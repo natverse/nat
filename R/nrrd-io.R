natverse/nat

commit 3bf9d4b4b2c05b17635a16cfb466e7174b497ea2

add function to prepare nrrd header from im3d

R/nrrd-io.R
@@ -362,6 +362,18 @@ write.nrrd<-function(x, file, enc=c("gzip","raw","text"),
   }
 }
 
+# internal function to make key spatial nrrd header fields from im3d object
+im3d2nrrdheader<-function(x) {
+  if(!is.im3d(x)) stop("x is not an im3d object!")
+  h=list(dimension=length(dim(x)), sizes=dim(x))
+  # for im3d assume that space dimension is same as array dimension
+  h$`space dimension`=dim(x)
+  # nb not origin(x) since that will return (0,0,0) if missing
+  h$`space origin`=attr(x, 'origin')
+  h$`space directions`=diag(voxdims(x))
+  h
+}
+
 .standardNrrdType<-function(type){
   if(type%in%c("float","double","block")) return (type)
   if(type%in%c("signed char", "int8", "int8_t")) return("int8")

commit 248603ec4bf7ba7c0992bf192cad40b9d87dd277

fix space dims when using write.nrrd on im3d
* this bug was introduced in commit 3bf9d4b
* include a test for this

R/nrrd-io.R
@@ -452,12 +452,30 @@ write.nrrd.header <- function (header, file) {
   cat("\n", file=file, append=TRUE)
 }
 
+#' @description \code{write.nrrd.header.for.file} makes a detached nrrd (nhdr) 
+#'   file to make another image type on disk compatible with the nrrd library.
+#' @rdname write.nrrd
+write.nrrd.header.for.file<-function(infile, outfile=NULL) {
+  if(is.null(outfile)) 
+    outfile=paste0(tools::file_path_sans_ext(infile),".nhdr")
+  x=read.im3d(infile, ReadData = FALSE)
+  if(!is.null(dd<-attr(x,'dataDef'))){
+    if(dd$HxType!='raw')
+      stop("only raw format Amiramesh files are nrrd compatible!")
+    if(nrow(dd)>1)
+      stop("I only accept Amiramesh files with one data block")
+    write.nrrd(x, outfile, enc = 'raw', dtype = dd$SimpleType, endian = dd$endian, 
+               datafile = infile, header=list(lineskip=dd$LineOffsets))
+  }
+  outfile
+}
+
 # internal function to make key spatial nrrd header fields from im3d object
 im3d2nrrdheader<-function(x) {
   if(!is.im3d(x)) stop("x is not an im3d object!")
   h=list(dimension=length(dim(x)), sizes=dim(x))
   # for im3d assume that space dimension is same as array dimension
-  h$`space dimension`=dim(x)
+  h$`space dimension`=length(dim(x))
   # nb not origin(x) since that will return (0,0,0) if missing
   h$`space origin`=attr(x, 'origin')
   h$`space directions`=diag(voxdims(x))

tests/testthat/test-im3d.R
@@ -59,8 +59,12 @@ test_that("round trip test for im3d is successful",{
   tf3=tempfile(tmpdir = td, fileext='.nhdr')
   # also check detached nrrd
   expect_is(write.im3d(d, tf3), 'character')
-  expect_equal(read.im3d(tf3), d, tol=1e-6)
+  expect_equal(d3<-read.im3d(tf3), d, tol=1e-6)
   expect_true(file.exists(sub("\\.nhdr$",".raw.gz",tf3)))
+  
+  # check nrrd header fields as well in detail
+  h1=attr(d,'header')
+  expect_equal(attr(d3,'header')[names(h1)], h1)
 })
 
 context("im3d")