natverse/nat

commit 3be07a2ad8ce80ac72b8da31ac15b5ded76bf29f

by simplifying mirror_brain.neuronlist we can add soma transformation
* note that mirroring could copy attributes (such as regtemplate) even if a general xform does not
* that said, we will nearly always use mirror_brain when we care about regtemplate

R/xform.R
@@ -488,5 +488,5 @@ mirror.default<-function(x, mirrorAxisSize, mirrorAxis=c("X","Y","Z"),
 #' @rdname mirror
 #' @seealso \code{\link{nlapply}}
 mirror.neuronlist<-function(x, subset=NULL, OmitFailures=NA, ...){
-  nlapply(x, FUN=mirror, ..., subset=subset, OmitFailures=OmitFailures)
+  mirror(x, subset=subset, OmitFailures=OmitFailures, ...)
 }

commit 98748355149fab29f34e90b21f591f72e4ca11d8

Fix mirror.neuronlist bug introduced in 3be07a2a
* was getting infinite recursion

R/xform.R
@@ -497,5 +497,5 @@ mirror.default<-function(x, mirrorAxisSize, mirrorAxis=c("X","Y","Z"),
 #' @rdname mirror
 #' @seealso \code{\link{nlapply}}
 mirror.neuronlist<-function(x, subset=NULL, OmitFailures=NA, ...){
-  mirror(x, subset=subset, OmitFailures=OmitFailures, ...)
+  NextMethod()
 }