375ffbfc26901b8671ca766052f7fbbdf6c39d5a
Gregory Jefferis <jefferis@gmail.com>
2016-10-27 14:00:44 +0100

    Don't build cmtk directly in home directory
    * otherwise it seems to get mixed up with package directory

--- build-cmtk.sh
+++ build-cmtk.sh
@@ -2,6 +2,7 @@
 set -e
 # check to see if cmtk install folder is empty
 if [ ! -d "$HOME/usr/local/bin" ]; then
+  mkdir -p $HOME/src && cd $HOME/src
   git clone --depth 50 https://github.com/jefferis/cmtk
   cd cmtk/core && mkdir build && cd build && cmake .. && make DESTDIR=$HOME/ all install
 else
