39e93edadeaee528ee2b6d881ce0506f31d742f2
Gregory Jefferis <jefferis@gmail.com>
2017-05-19 20:06:54 +0100

    update build_docs script
    * but note that it does not build in quite the same way as from inside rstudio
      maybe due to the interactive setting

--- build_docs.r
+++ build_docs.r
@@ -1,6 +1,8 @@
-#!/usr/bin/Rscript --no-restore --slave
+#!/usr/local/bin/Rscript --no-restore --slave
 options(repos="https://cran.rstudio.com/")
 options(pager='cat')
+# we don't want spurious progress bars everywhere
+options(nat.progress="none")
 message("interactive is ", interactive())
 library(pkgdown)
 args=commandArgs(trailingOnly = TRUE)
