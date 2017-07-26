c469556d9c6d7ce68bd869e4cf1028f23a4897dd
Gregory Jefferis <jefferis@gmail.com>
2017-07-26 23:06:41 +0100

    as.mesh3d.ashape3d correct order triangle vertices
    * was causing trouble for pointsinside
    * fixes #349

--- R/alphashape3d.R
+++ R/alphashape3d.R
@@ -5,9 +5,16 @@
 #' @details An \href{https://en.wikipedia.org/wiki/Alpha_shape}{alpha shape} is 
 #'   a generalisation of a convex hull enclosing a set of points. Unlike a 
 #'   convex hull, the resultant surface can be partly concave allowing the 
-#'   surface to more closely follow the set of points. The parameter alpha 
-#'   
-#' @return a \code{\link[rgl]{mesh3d}} object which can be plotted and 
+#'   surface to more closely follow the set of points.
+#'
+#'   In this implementation, the parameter alpha is a scale factor with units of
+#'   length that defines a spatial domain. When alpha is larger the alpha shape
+#'   approaches the convex hull; when alpha is smaller the alpha shape has a
+#'   greater number of faces / vertices i.e. it follows the points more closely.
+#' @param tri_to_keep Which alphashape triangles to keep (expert use only - see
+#'   \code{triang} entry in \bold{Value} section of
+#'   \code{\link[alphashape3d]{ashape3d}} docs for details.)
+#' @return a \code{\link[rgl]{mesh3d}} object which can be plotted and
 #'   manipulated using \code{\link{rgl}} and \code{nat} packages.
 #' @export
 #' 
@@ -21,28 +28,68 @@
 #' 
 #' # convert to mesh3d
 #' kcs20.mesh=as.mesh3d(kcs20.a)
+#'
+#' # check that all points are inside mesh
+#' all(pointsinside(kcs20, kcs20.mesh))
+#' # and show that we can also use the alphashape directly
+#' all(pointsinside(kcs20, kcs20.a))
+#'
 #' clear3d()
 #' wire3d(kcs20.mesh)
 #' plot3d(kcs20, col=type, lwd=2)
 #' }
-as.mesh3d.ashape3d <- function(x, ...) {
-  if (length(x$alpha) > 1)
+as.mesh3d.ashape3d <- function(x, tri_to_keep=2L, ...) {
+   if (length(x$alpha) > 1)
     stop("I don't know how to handle ashape3d objects with >1 alpha value")
   iAlpha = 1
 
+  if(!all(tri_to_keep %in% 0:3))
+    stop("tri_to_keep must contain values in the range 0:3 only!")
   # from help for ashape3d
   # for each alpha, a value (0, 1, 2 or 3) indicating, respectively, that the
   # triangle is not in the alpha-shape or it is interior, regular or singular
   # (columns 9 to last)
 
   # Pick the rows for which the triangle is regular or singular
-  selrows = x$triang[, 8 + iAlpha] >= 2
+  selrows = x$triang[, 8 + iAlpha] %in% tri_to_keep
   tr <- x$triang[selrows, c("tr1", "tr2", "tr3")]
 
-  rgl::tmesh3d(
+  m=rgl::tmesh3d(
     vertices = t(x$x),
     indices = t(tr),
     homogeneous = FALSE,
     ...
   )
+
+  # now let's fix triangle order
+  # calculate face normals for alphashape
+  sn=alphashape3d::surfaceNormals(x, indexAlpha = iAlpha)
+  # ... and new mesh3d object
+
+  fn=facenormals(m)
+  # calculate dot products between the two normals
+  dp=dotprod(fn, sn$normals)
+
+  # flip the vertex order when normals are in opposite directions
+  m$it[,dp<0]=m$it[c(1,3,2),dp<0]
+  m
+}
+
+# cross product of two Nx3 matrices (where each row is a 3D vector)
+xprodm <- function(u, v) {
+  cbind(u[, 2] * v[, 3] - u[, 3] * v[, 2],
+        u[, 3] * v[, 1] - u[, 1] * v[, 3],
+        u[, 1] * v[, 2] - u[, 2] * v[, 1])
+}
+
+# face normals of a trimesh
+facenormals <- function(x, ...) {
+  v=xyzmatrix(x)
+  if(!length(x$it))
+    stop("I only work for trimeshes")
+
+  v1=v[x$it[1, ], ]
+  v2=v[x$it[2, ], ]
+  v3=v[x$it[3, ], ]
+  xprodm(v1-v3, v2-v1)
 }
--- tests/testthat/test-hxsurf.R
+++ tests/testthat/test-hxsurf.R
@@ -111,7 +111,12 @@ test_that("we can convert ashape3d to rgl::mesh3d",{
   skip_if_not_installed('alphashape3d')
   kcs20.a=alphashape3d::ashape3d(xyzmatrix(kcs20), alpha = 10)
   expect_is(as.mesh3d(kcs20.a), 'mesh3d')
+  
+  skip_if_not_installed("Rvcg")
+  # also shows that we can use the alphashape directly in pointsinside
+  expect_true(all(pointsinside(kcs20, kcs20.a)))
 })
+
 
 test_that("we can save and re-read hxsurf object", {
   surffile <- tempfile()
@@ -133,15 +138,15 @@ test_that("we can xform hxsurf object using registration", {
 })
 }
 
-if(require('Rvcg')){
-  test_that('check if points are inside a surface',{
-    # the surface and neuron are registered against different templates
-    # so cheat by using an approximate offset
-    n=kcs20[[1]]+c(40,-30,20)
-    MB_CA_L=readRDS("testdata/amira/JFRC2_MB_CA_L.rds")
-    expect_equal(sum(pointsinside(n, MB_CA_L)), 55L)
-    surf2=read.hxsurf(surf_file, RegionChoice="both")
-    MB_CA_L=subset(surf2, "MB_CA_L")
-    expect_equal(sum(pointsinside(n, MB_CA_L)), 55L)
-  })
-}
+
+test_that('check if points are inside a surface',{
+  skip_if_not_installed("Rvcg")
+  # the surface and neuron are registered against different templates
+  # so cheat by using an approximate offset
+  n=kcs20[[1]]+c(40,-30,20)
+  MB_CA_L=readRDS("testdata/amira/JFRC2_MB_CA_L.rds")
+  expect_equal(sum(pointsinside(n, MB_CA_L)), 55L)
+  surf2=read.hxsurf(surf_file, RegionChoice="both")
+  MB_CA_L=subset(surf2, "MB_CA_L")
+  expect_equal(sum(pointsinside(n, MB_CA_L)), 55L)
+})
