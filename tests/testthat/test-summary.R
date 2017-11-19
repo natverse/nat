452b7c313bbc17841f04cee138e77e92e7711672
Gregory Jefferis <jefferis@gmail.com>
2017-11-19 16:14:32 +0000

    Add test for summary.neuron

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,6 +1,7 @@
 context("summary")
 
 test_that("summary.neuronlist behaves", {
-  expect_is(summary(Cell07PNs), 'data.frame')
+  expect_is(s <- summary(Cell07PNs), 'data.frame')
+  expect_known_value(s, file = 'testdata/summary_cell07pns.rda')
   expect_is(summary(kcs20, veclength=1.2), 'data.frame')
 })
