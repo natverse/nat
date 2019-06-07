a72282e5d180bc2351af5fa64bba556022ee0933
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-06-07 20:35:59 +0100

    Added make_model
    * Generate a 3D model from connector and/or tree node data

--- R/interactive.R
+++ R/interactive.R
@@ -3,7 +3,7 @@
 #' Prune a neuron interactively in an rgl window
 #'
 #' @description Remove points from a neuron, keeping the root node intact
-#'
+#' @param ... Additional methods passed to \code{prune_vertices}
 #' @inheritParams prune
 #' @return A pruned neuron/neuronlist object
 #' @examples
@@ -24,7 +24,7 @@ prune_online.neuron <- function(x, ...){
     selected = select_points(xyzmatrix(x), plot3d = x)
     v = match(data.frame(t(selected)), data.frame(t(xyzmatrix(x))))
     neuron = prune_vertices(x,verticestoprune=v,invert=TRUE)
-    rgl::clear3d();rgl::plot3d(neuron, col ="black",...)
+    rgl::clear3d();rgl::plot3d(neuron, col ="black")
     continue = readline("Finished with this neuron? yes/no ")
   }
   neuron
@@ -32,7 +32,7 @@ prune_online.neuron <- function(x, ...){
 #' @export
 #' @rdname prune_online
 prune_online.neuronlist <- function(x, ...){
-  nat::nlapply(x,prune_online.neuron)
+  nlapply(x,prune_online.neuron)
 }
 
 #' Manually assign a dendrite and axon to a neuron
@@ -41,7 +41,6 @@ prune_online.neuronlist <- function(x, ...){
 #'
 #' @param x a neuron/neuronlist object
 #' @param soma whether or not to plot a soma, and at what radius
-#' @param ... additional arguments passed to methods
 #' @return The neuron/neuronlist object with axon/dendrite info assigned in SWC format to neuron$d
 #' #' @seealso \code{\link{as.neuron.ngraph}}, \code{\link{subset.neuron}}, 
 #'   \code{\link{prune.neuron}}, \code{\link{prune_online}}
@@ -52,10 +51,10 @@ prune_online.neuronlist <- function(x, ...){
 #' } 
 #' @export
 #' @rdname manually_assign_axon_dendrite
-manually_assign_axon_dendrite <-function(x, ...) UseMethod("manually_assign_axon_dendrite")
+manually_assign_axon_dendrite <-function(x) UseMethod("manually_assign_axon_dendrite")
 #' @export
 #' @rdname manually_assign_axon_dendrite
-manually_assign_axon_dendrite.neuron <- function(x, ...){
+manually_assign_axon_dendrite.neuron <- function(x){
   happy = "no"
   x$d$Label = 0
   while(!happy%in%c("y","yes")){
@@ -67,7 +66,7 @@ manually_assign_axon_dendrite.neuron <- function(x, ...){
     message("Please choose axon for your neuron ")
     axon = prune_online.neuron(x)
     x$d$Label[x$d$PointNo%in%axon$d$PointNo] = 2
-    x$d$Label[nat::rootpoints(x)] = 1
+    x$d$Label[rootpoints(x)] = 1
     rgl::clear3d()
     rgl::plot3d(dend,col="blue")
     rgl::plot3d(axon, col = "orange")
@@ -78,8 +77,8 @@ manually_assign_axon_dendrite.neuron <- function(x, ...){
 }
 #' @export
 #' @rdname manually_assign_axon_dendrite
-manually_assign_axon_dendrite.neuronlist<-function(x, ...){
-  nat::nlapply(x, manually_assign_axon_dendrite.neuron, ...)
+manually_assign_axon_dendrite.neuronlist<-function(x){
+  nlapply(x, manually_assign_axon_dendrite.neuron)
 }
 
 #' Interactively re-root neurons to their soma
@@ -90,7 +89,6 @@ manually_assign_axon_dendrite.neuronlist<-function(x, ...){
 #' @param someneuronlist a neuron/neuronlist object
 #' @param brain for context, plot some other objects (e.g. a brain from the nat.templatebrains package such as FCWB,
 #' or any object that may be plotted using plot3d)
-#' @param ... additional arguments passed to methods
 #' @examples
 #' \dontrun{ 
 #' ## Admittedly, these neurns have their somata chopped off! 
@@ -98,7 +96,7 @@ manually_assign_axon_dendrite.neuronlist<-function(x, ...){
 #' } 
 #' @return a matrix of 3D points
 #' @export
-correct_soma <- function(someneuronlist, brain = NULL,...){
+correct_soma <- function(someneuronlist, brain = NULL){
   correctedsomas = neuronlist()
   nopen3d()
   for (n in 1:length(someneuronlist)){
@@ -107,14 +105,14 @@ correct_soma <- function(someneuronlist, brain = NULL,...){
     print(names(someneuronlist[n]))
     if(!is.null(brain)){rgl::plot3d(brain)}
     rgl::plot3d(w, soma = T)
-    eps.xyz=w$d[nat::endpoints(w),]
+    eps.xyz=w$d[endpoints(w),]
     progress =F
     while(progress == F){
       cat ("Rotate brain and then hit [enter] to continue")
       line <- readline()
       message("Select new root from highlighted endpoints")
       selected.point <- select3d()
-      selected.point <- selected.point(nat::xyzmatrix(eps.xyz))
+      selected.point <- selected.point(xyzmatrix(eps.xyz))
       selected.point <- eps.xyz$PointNo[selected.point]
       if (length(selected.point)>1|length(selected.point)==0){
         message("Multiple end points selected, try again")
@@ -129,4 +127,79 @@ correct_soma <- function(someneuronlist, brain = NULL,...){
   }
   correctedsomas[,] = someneuronlist[,]
   correctedsomas
+}
+
+
+#' Generate a 3D model from connector and/or tree node data
+#'
+#' @description Generate a mesh3d model based on points contained in a neuronlist or neuron object
+#'
+#' @param someneuronlist a neuronlist or neuron object
+#' @param substrate whether to make the model based off of connectors, neuron cable or both
+#' @param auto.selection whether to try and remove points based on interactively chosen values for 'groupsize' and 'maxdistance'
+#' @param maxdistance for automated cluster identification. Maximum distance at which nodes can be part of a cluster
+#' @param groupsize an integer number of nearest neighbours to find using nabor::knn()
+#' @param selection whether or not to interactively select values for maxdistance and groupsize.
+#' @param alpha a single value or vector of values for α, fed to \code{alphashape3d::\link{ashape3d}}. Selection is subsequently interactive
+#' @param chosen.points a matrix of 3D points. Use this argument if you do not want to interactively select the 3D fed to \code{alphashape3d::\link{ashape3d}}
+#' @examples
+#' \dontrun{ 
+#' # Make a model based off of fly olfactory projection neuron arbours
+#' PN_blob = make_model(Cell07PNs)
+#' } 
+#' @seealso \code{\link{prune_online}}
+#' @return A mesh3d object
+#' @export
+make_model <- function(someneuronlist, substrate = c("connectors","cable", "both"), maxdistance = 10, groupsize = 10, alpha = 30, auto.selection = TRUE, chosen.points = NULL){
+  if (substrate=="connectors"){synapse.points<-xyzmatrix(do.call(rbind, lapply(someneuronlist, function(x) x$connectors)))
+  }else if(substrate =="cable"){synapse.points<-xyzmatrix(someneuronlist)
+  }else if (substrate == "both"){synapse.points<-rbind(xyzmatrix(someneuronlist), xyzmatrix(catmaid::connectors(someneuronlist)))}
+  rgl::open3d()
+  if (auto.selection == TRUE){
+    progress = "n"
+    while (progress == "n"){
+      groupsize <- as.numeric (readline(prompt="Select a value for the cluster groupsize  "))
+      maxdistance <- as.numeric (readline(prompt="Select a value for maximum distance between points  "))
+      neighbours<-nabor::knn(synapse.points, synapse.points, k = groupsize)
+      loose <- apply(neighbours$nn.dists, 1, function(x) {(any(as.numeric(x[1:ncol(neighbours$nn.dists)]) > maxdistance))})
+      keep<-c(neighbours$nn.idx[,1][!loose])
+      close.points<-synapse.points[keep,]
+      rgl::clear3d();rgl::points3d(close.points, cl = 'black'); rgl::points3d(synapse.points, col = 'red')
+      progress = readline(prompt="Continue? y/n  ")
+    }
+  }
+  else{
+    neighbours<-nabor::knn(synapse.points, synapse.points, k = groupsize)
+    loose <- apply(neighbours$nn.dists, 1, function(x) {(any(as.numeric(x[1:ncol(neighbours$nn.dists)]) > maxdistance))})
+    keep<- c(neighbours$nn.idx[,1][!loose])
+    close.points<-synapse.points[keep,]
+    rgl::clear3d();rgl::points3d(close.points, cl = 'black'); rgl::points3d(synapse.points, col = 'red')
+  }
+  # Manual point deselection
+  selected.points = unique(close.points)
+  if (!is.null(chosen.points)){ selected.points = chosen.points}
+  progress = readline(prompt="Remove (r) or add (a) points? Or continue to alphashape generation (c)?  ")
+  while (progress != "c"){
+    if (progress == 'r'){
+      remove.points <- rgl::select3d()
+      removed.points <- remove.points(selected.points)
+      selected.points<-subset(selected.points, !removed.points)
+      rgl::clear3d(); rgl::points3d(selected.points); rgl::points3d(synapse.points, col = 'red')
+    }
+    if (progress == 'a'){
+      add.points <- rgl::select3d()
+      added.points<-subset(synapse.points, add.points(synapse.points))
+      selected.points<-rbind(selected.points, added.points)
+      rgl::clear3d(); rgl::points3d(selected.points); rgl::points3d(synapse.points, col = 'red')
+    }
+    progress = readline(prompt="Remove (r), add (a) or save (s) points?  ")
+  }
+  progress = "n"
+  while (progress == "n"){
+    alpha <- as.numeric (readline(prompt="Select a nmeric value for alpha  "))
+    alphashape<-alphashape3d::ashape3d(unique(selected.points), alpha = alpha)
+    plot(alphashape)
+    progress<-readline(prompt="Continue? y/n  ")
+  }
+  as.mesh3d(alphashape)
 }
