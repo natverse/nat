7f32d07d25e06b3982765bb69b30b2409f340c74
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-08-06 20:36:33 +0100

    put prune_online mthods back

--- R/interactive.R
+++ R/interactive.R
@@ -21,7 +21,8 @@
 #' @export
 #' @rdname prune_online
 prune_online <-function(x, ...) UseMethod("prune_online")
-
+#' @export
+#' @rdname prune_online
 prune_online.neuron <- function(x, ...){
   continue = "no"
   ids=integer()
@@ -36,7 +37,8 @@ prune_online.neuron <- function(x, ...){
   }
   neuron
 }
-
+#' @export
+#' @rdname prune_online
 prune_online.neuronlist <- function(x, ...){
   nlapply(x,prune_online.neuron)
 }
