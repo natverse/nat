51c89d9576dd1e2013cf7602cac109374bdfa51b
Gregory Jefferis <jefferis@gmail.com>
2021-03-16 23:58:24 +0000

    Teach neuronlistz about embedded data.frame

--- R/neuronlistz.R
+++ R/neuronlistz.R
@@ -1,19 +1,45 @@
 #' A neuronlist object that will read neurons from a zip file on demand
 #'
-#' @param inzip 
-#' @param patt 
-#' @param df 
-#' @param ... 
+#' @details \code{\link{neuronlistz}} is designed to wrap zip files containing
+#'   neurons saved in the RDS or faster/smaller qs format for rapid access. You
+#'   should be able to read typical files in <20 ms. For files of ~3 GB there is
+#'   a fixed cost of the order of 10-15ms per read.
 #'
-#' @return
+#' @param zip Path to the zip file
+#' @param patt Optional regex pattern or function to specify a subset of files.
+#' @param df A data.frame of metadata that will be attached to the
+#' @param ... Additional arguments currently ignored
+#'
+#' @return A \code{\link{neuronlist}} object with additional class
+#'   \code{neuronlistz}
+#' @seealso \code{\link{neuronlist}}, \code{\link{neuronlistfh}},
+#'   \code{\link{write.neurons}}
 #' @export
 #'
 #' @examples
-neuronlistz <- function(inzip, patt=NULL, df=NULL, ...) {
+#' \donttest{
+#' write.neurons(Cell07PNs[1:5], tf <- tempfile(fileext = '.zip'), format='rds')
+#' nz=neuronlistz(tf)
+#' nz[[1]]
+#' nz[1:5]
+#' }
+#' \dontrun{
+#' write.neurons(Cell07PNs[1:5], tf <- tempfile(fileext = '.zip'), format='qs')
+#' nz2=neuronlistz(tf)
+#' all.equal(nz2[1:3], nz[1:3])
+#' }
+neuronlistz <- function(zip, patt=NULL, df=NULL, ...) {
   stopifnot(requireNamespace("zip", quietly = TRUE))
-  inzip=path.expand(inzip)
-  zl=zip::zip_list(inzip)
+  zip=path.expand(zip)
+  zl=zip::zip_list(zip)
   ff=zl$filename
+  
+  dff="write.neurons.dataframe.rds"
+  if(dff %in% ff) {
+    ff=setdiff(ff, dff)
+    if(is.null(df))
+      df <- read_from_zip(zip, dff, neuron=FALSE)
+  }
   
   keyfilemap <- if(!is.null(patt)) {
     if(is.character(patt)) ff[grepl(patt, ff)]
@@ -37,19 +63,21 @@ neuronlistz <- function(inzip, patt=NULL, df=NULL, ...) {
     df=data.frame(id=names(keyfilemap), stringsAsFactors = FALSE)
     rownames(df)=df[['id']]
   }
-  attr(nlf,'db')=inzip
+  attr(nlf,'db')=zip
   attr(nlf,'df')=df
   attr(nlf,'options')=options
   class(nlf)=c('neuronlistz','neuronlist',class(nlf))
   nlf
 }
 
-read_from_zip <- function(zipfile, p, multi=FALSE) {
+read_from_zip <- function(zipfile, p, multi=FALSE, neuron=TRUE) {
   td <- tempfile()
   on.exit(unlink(td, recursive = TRUE))
   zip::unzip(zipfile, p, exdir = td)
   paths=file.path(td, p)
-  n=if(multi) read.neurons(paths) else read.neuron(paths)
+  n <- if(multi) read.neurons(paths) else {
+    if(neuron) read.neuron(paths) else readRDS(paths)
+  }
   n
 }
 
@@ -84,5 +112,5 @@ read_from_zip <- function(zipfile, p, multi=FALSE) {
   l <- read_from_zip(zipfile, p, multi = T)
   
   new=as.neuronlist(l, df=attr(x, 'df')[ichecked, , drop=FALSE])
-  nat:::copy_nl_attributes(new, x, ignoremore=c("keyfilemap", "db", "options"))
+  copy_nl_attributes(new, x, ignoremore=c("keyfilemap", "db", "options"))
 }
