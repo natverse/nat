88d3d00cddc74540c514acd06f2edac2570c4abe
Gregory Jefferis <jefferis@gmail.com>
2024-08-09 22:43:36 +0100

    switch to natdev branch in build-cmtk.sh

--- build-cmtk.sh
+++ build-cmtk.sh
@@ -3,7 +3,7 @@ set -e
 # check to see if cmtk install folder is empty
 if [ ! -d "$HOME/usr/local/bin" ]; then
   mkdir -p $HOME/src && cd $HOME/src
-  git clone --depth 50 https://github.com/jefferis/cmtk
+  git clone --depth 50 https://github.com/jefferis/cmtk@natdev
   cd cmtk/core && mkdir build && cd build && cmake .. && make DESTDIR=$HOME/ all install
 else
   echo 'Using cached $HOME/usr/local directory.';
