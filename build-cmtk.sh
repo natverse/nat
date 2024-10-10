8db0c23228962a8eabefd2aba5069956b0c8d497
Gregory Jefferis <jefferis@gmail.com>
2024-10-10 19:08:19 +0100

    need to specify cmtk branch in git clone
    * otherwise it's not available

--- build-cmtk.sh
+++ build-cmtk.sh
@@ -3,7 +3,7 @@ set -e
 # check to see if cmtk install folder is empty
 if [ ! -d "$HOME/usr/local/bin" ]; then
   mkdir -p $HOME/src && cd $HOME/src
-  git clone --depth 50 https://github.com/jefferis/cmtk
+  git clone --depth 10 --branch natdev https://github.com/jefferis/cmtk
   cd cmtk && git checkout natdev
   cd core && mkdir build && cd build && cmake .. && make DESTDIR=$HOME/ all install
 else
