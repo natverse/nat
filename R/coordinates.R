879a185acb9b04dd61ff19de3e5ad6fda0001bbb
Gregory Jefferis <jefferis@gmail.com>
2025-10-13 09:56:53 +0100

    use matrixStats::colRanges to save more memory
    > bench::mark(v1=coord2ind(mdf, testImage, version=1), v2=coord2ind(mdf, testImage, version=2), v3=coord2ind(mdf, testImage, version=3), min_time = 1)
    # A tibble: 3 × 13
      expression     min median          `itr/sec` mem_alloc           `gc/sec` n_itr  n_gc total_time
      <bch:expr> <bch:t> <bch:>              <dbl> <bch:byt>              <dbl> <int> <dbl>   <bch:tm>
    1 v1          82.9ms 90.2ms 7.93571545940208e0     298MB 1.78553597836547e1     8    18      1.01s
    2 v2          68.6ms   80ms 9.53099788536415e0     252MB 1.90619957707283e1    10    20      1.05s
    3 v3          48.6ms 60.1ms 1.44604097192445e1     172MB 1.9280546292326 e1    15    20      1.04s
    # ℹ 4 more variables: result <list>, memory <list>, time <list>, gc <list>

--- R/coordinates.R
+++ R/coordinates.R
@@ -112,7 +112,7 @@ coord2ind <- function(coords, ...) UseMethod("coord2ind")
 #' @param origin the origin of the 3D image.
 #' @param linear.indices Whether or not to convert the voxel indices into a 
 #'   linear 1D form (the default) or to keep as 3D indices.
-#' @param use.scale For testing an alternative algorithm
+#' @param version For testing alternative algorithms
 #' @param aperm permutation order for axes.
 #' @param Clamp Whether or not to map out of range coordinates to the nearest 
 #'   in range index (default \code{FALSE})
@@ -121,7 +121,7 @@ coord2ind <- function(coords, ...) UseMethod("coord2ind")
 #' @export
 #' @rdname coord2ind
 coord2ind.default<-function(coords, imdims, voxdims=NULL, origin=NULL, 
-                            linear.indices=TRUE, aperm=NULL, use.scale=FALSE,
+                            linear.indices=TRUE, aperm=NULL, version=1L,
                             Clamp=FALSE, CheckRanges=!Clamp, ...){
   if(is.object(imdims)){
     if(!inherits(imdims, "im3d"))
@@ -148,7 +148,7 @@ coord2ind.default<-function(coords, imdims, voxdims=NULL, origin=NULL,
       stop("coordinates should be an N x 3 matrix-like object")
   }
   
-  if(use.scale) {
+  if(version>=2) {
     if(missing(origin) || is.null(origin) || all(origin==0))
       origin=FALSE
     pixcoords=round(scale(coords, center = origin, scale = voxdims))+1L
@@ -164,7 +164,11 @@ coord2ind.default<-function(coords, imdims, voxdims=NULL, origin=NULL,
     pixcoords[,2]=pmin(imdims[2],pmax(1,pixcoords[,2]))
     pixcoords[,3]=pmin(imdims[3],pmax(1,pixcoords[,3]))
   } else if(CheckRanges){
-    ranges=apply(pixcoords,2,range)
+    if(version>=3 && requireNamespace('matrixStats', quietly = T))
+      ranges=t(matrixStats::colRanges(pixcoords))
+    else {
+      ranges=apply(pixcoords,2,range)
+    }
     if(any(ranges[2,]>imdims) || any(ranges[1,]<1))
       stop("pixcoords out of range")
   }
